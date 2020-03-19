6b40b1fe25a006bc607706f5422798b2c99de923
shirayner <1766211120@qq.com>
2020-03-20 00:39:44 +0800

    [ADD] SpringCache_01_ConcurrentMap

--- sample-cache/spring-boot-cache-concurrentmap/src/main/java/com/ray/study/smaple/cache/ConcurrentMapCacheApplication.java
+++ sample-cache/spring-boot-cache-concurrentmap/src/main/java/com/ray/study/smaple/cache/ConcurrentMapCacheApplication.java
@@ -10,8 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author ray
  * @date 2020/3/19
  */
-@SpringBootApplication(scanBasePackages={"com.ray.study.smaple.cache",
-        "com.ray.study.smaple.sb.data.jpa.repository"})
+@SpringBootApplication
 public class ConcurrentMapCacheApplication {
 
     public static void main(String[] args) {
